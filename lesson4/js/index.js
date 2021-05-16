33c22e5ebcc3f5faec4b1822fc0eeb77b8415743
misatovelazquez <misatovelazquez@gmail.com>
2021-05-15 20:31:45 -0500

    responsive done

--- lesson4/js/index.js
+++ lesson4/js/index.js
@@ -1,3 +1,12 @@
 document.querySelector('#lastU').textContent= document.lastModified;
 document.getElementById("copyright").innerHTML = (new Date().getFullYear());
- 
+
+
+const hambutton= document.querySelector('.ham');
+const mainnav = document.querySelector('.navigation');
+
+hambutton = addEventListener('click',() =>
+{mainnav.classList.toggle('responsive')}, false);
+
+window.onresize = () =>{if (window.innerWidth > 760)
+mainnav.classList.remove('responsive')};
