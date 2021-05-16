c335c22060a4bd6a98590631e0be7ecb66da3e2a
misatovelazquez <misatovelazquez@gmail.com>
2021-05-16 01:12:39 -0500

    added span id

--- lesson4/js/date.js
+++ lesson4/js/date.js
@@ -1,5 +1,5 @@
 
-const datefield = document.querySelector("date");
+const datefield = document.querySelector("#date");
 
 const now = new Date();
 const fulldate = new Intl.DateTimeFormat("en-US",{ dateStyle: 'full' , formatMatcher:"best fit"}).format(now);
