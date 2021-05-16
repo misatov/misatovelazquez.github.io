b970bced8f113a6eec2c966df31af5b82e5ec93d
misatovelazquez <misatovelazquez@gmail.com>
2021-05-16 01:08:37 -0500

    dateStyle changed to camel case

--- lesson4/js/date.js
+++ lesson4/js/date.js
@@ -2,6 +2,6 @@
 const datefield = document.querySelector("date");
 
 const now = new Date();
-const fulldate = new Intl.DateTimeFormat("en-US",{ datestyle: 'full' }).format(now);
+const fulldate = new Intl.DateTimeFormat("en-US",{ dateStyle: 'full' , formatMatcher:"best fit"}).format(now);
 
 datefield.textContent = fulldate;
