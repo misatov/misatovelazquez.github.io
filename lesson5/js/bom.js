af94ee409375cc8a5b8b036084854715c579e6c4
misatovelazquez <misatovelazquez@gmail.com>
2021-05-20 16:17:11 -0500

    comment updated

--- lesson5/js/bom.js
+++ lesson5/js/bom.js
@@ -1,21 +1,30 @@
+//create list
 const list = document.querySelector('ul');
+//provide input box
 const input = document.querySelector('input');
+//add chapter button
 const button = document.querySelector('button');
 
+//click to add list using event listener
 button.addEventListener('click', () => {
   let myItem = input.value;
   input.value = '';
 
+  //create li element
   const listItem = document.createElement('li');
+  // create text in span
   const listText = document.createElement('span');
+  //create delete button 
   const listBtn = document.createElement('button');
 
+  //Use appendChild to output listItem, listText and listBtn
   listItem.appendChild(listText);
   listText.textContent = myItem;
   listItem.appendChild(listBtn);
   listBtn.textContent = '❌';
   list.appendChild(listItem);
 
+  //click to remove from list using event listener
   listBtn.addEventListener('click', () => {
     list.removeChild(listItem);
   })
