ea9b201e662872a41588a41ad31c9f2bda9f0f0c
misatovelazquez <misatovelazquez@gmail.com>
2021-05-25 21:37:03 -0500

    ternary operator changed

--- lesson5/js/banner.js
+++ lesson5/js/banner.js
@@ -2,16 +2,16 @@
 // console.log(thedate.getDay());
 
 // // process - if it is friday then turn the aside display property to block
-if(now.getDay() == 5){
-    // console.log('Pancake day!! 💛');
-   document.querySelector('#banner').style.display = 'block';
-}
-else{
-    document.querySelector('#banner').style.display = 'none';
-}
+// if(now.getDay() == 5){
+//     // console.log('Pancake day!! 💛');
+//    document.querySelector('#banner').style.display = 'block';
+// }
+// else{
+//     document.querySelector('#banner').style.display = 'none';
+// }
 
 // *challenge* try in one statement if possible 
 // 
-//  return (thedate.getDay() == 5 ) ? 'document.querySelector('#banner').style.display = "block"' : 'document.querySelector('#banner').style.display = "none"';
-// // 
-// console.log(thedate.getDay());
+now.getDay() == 5 ? document.querySelector('#banner').style.display = 'block' : document.querySelector('#banner').style.display = 'none';
+
+// console.log(now.getDay());
