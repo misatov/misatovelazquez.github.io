3e297c88b6d4892cd3b2123d436e28dc96c28967
misatovelazquez <misatovelazquez@gmail.com>
2021-05-25 23:02:04 -0500

    lesson6

---
+++ lesson6/js/banner.js
@@ -0,0 +1,17 @@
+// let thedate = new Date();
+// console.log(thedate.getDay());
+
+// // process - if it is friday then turn the aside display property to block
+// if(now.getDay() == 5){
+//     // console.log('Pancake day!! 💛');
+//    document.querySelector('#banner').style.display = 'block';
+// }
+// else{
+//     document.querySelector('#banner').style.display = 'none';
+// }
+
+// *challenge* try in one statement if possible 
+// 
+now.getDay() == 5 ? document.querySelector('#banner').style.display = 'block' : document.querySelector('#banner').style.display = 'none';
+
+// console.log(now.getDay());
