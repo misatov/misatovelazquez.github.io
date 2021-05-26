4e71a5a6305f4ebaff03efdd06e45ce8ebda529d
misatovelazquez <misatovelazquez@gmail.com>
2021-05-25 21:21:52 -0500

    banner js changed

--- lesson5/js/banner.js
+++ lesson5/js/banner.js
@@ -1,17 +1,17 @@
-let thedate = new Date();
+// let thedate = new Date();
 // console.log(thedate.getDay());
 
-// process - if it is friday then turn the aside display property to block
-if(thedate.getDay() == 5){
+// // process - if it is friday then turn the aside display property to block
+if(now.getDay() == 5){
     // console.log('Pancake day!! 💛');
    document.querySelector('#banner').style.display = 'block';
 }
 else{
-    document.querySelector('#banner').style.display = "none";
+    document.querySelector('#banner').style.display = 'none';
 }
 
 // *challenge* try in one statement if possible 
-// if(thedate.getDay() == 5){
-//  return (thedate.getDay() == 5 ? document.querySelector('#banner').style.display = 'block' : document.querySelector('#banner').style.display = "none");
-// }
+// 
+//  return (thedate.getDay() == 5 ) ? 'document.querySelector('#banner').style.display = "block"' : 'document.querySelector('#banner').style.display = "none"';
+// // 
 // console.log(thedate.getDay());
