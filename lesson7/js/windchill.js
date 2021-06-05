283b7275ae034e007c08805a9a22c780be210fd3
misatovelazquez <misatovelazquez@gmail.com>
2021-06-05 12:12:55 -0500

    files transfered from lesson 6

---
+++ lesson7/js/windchill.js
@@ -0,0 +1,13 @@
+const temp = document.querySelector('.temp').textContent;
+const speed = document.querySelector('.speed').textContent;
+
+let answer = parseFloat( 35.74 + 0.6215*temp - 35.75*Math.pow(speed,0.16) + 0.4275*temp*Math.pow(speed,0.16));
+
+if(temp<= 50 && speed>3){
+    document.querySelector('.windchill').textContent = answer.toFixed(0);
+}
+else{
+    document.querySelector('.windchill').textContent ="N/A";
+}
+
+
