0aa8c68665f7b0e9205b5c7809893ec06a9b87fd
misatovelazquez <misatovelazquez@gmail.com>
2021-06-19 22:09:35 -0500

    data done update portal

---
+++ lesson9/js/threetowns.js
@@ -0,0 +1,46 @@
+const requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
+
+fetch(requestURL)
+  .then(function (response) {
+    return response.json();
+  })
+
+
+  .then(function (jsonObject) {
+    console.table(jsonObject);  
+
+    const towns = jsonObject['towns'];
+    const onlyThree = towns.filter((towns => towns.name == 'Soda Springs' || towns.name == 'Fish Haven' || towns.name == 'Preston'  ));
+    console.log(onlyThree);
+
+    for(let i=0; i<towns.length; i++){
+        let card = document.createElement('section'); // container
+        let h3= document.createElement('h3');
+        let motto = document.createElement('p');
+        let year = document.createElement('p');
+        let population = document.createElement('p');
+        let rainFall = document.createElement('p');
+        let img = document.createElement('img');
+
+        h3.textContent = onlyThree[i].name ;
+        motto.textContent = onlyThree[i].motto ;
+        year.textContent = "Year Founded : " + onlyThree[i].yearFounded;
+        population.textContent = "Population : " + onlyThree[i].currentPopulation;
+        rainFall.textContent = "Average Rain Fall: " + onlyThree[i].averageRainfall;
+        img.setAttribute('src', `images/${onlyThree.photo}`);
+        // img.setAttribute('alt', `${prophets[i].name} ${prophets[i].lastname} - ${prophets[i].order}`);
+
+
+        card.append(h3);
+        card.append(motto);
+        card.append(year);
+        card.append(population);
+        card.append(rainFall);
+        card.append(img);
+
+
+        document.querySelector('div.herodiv').append(card);
+    }
+
+    });
+  
