943783ab06ef593f6df3f2b01dd753d9501a6877
misatovelazquez <misatovelazquez@gmail.com>
2021-06-27 00:41:05 -0500

    upload

--- lesson10/js/api.js
+++ lesson10/js/api.js
@@ -1,14 +1,31 @@
-const apiURL = "http://api.openweathermap.org/data/2.5/weather?id=5604473&appid=8bbd262e26fa22b9acfd7a7ad5b87217&units=imperial";
+const apiURLw = "http://api.openweathermap.org/data/2.5/weather?id=5604473&appid=1e25916dc15bb9ee780725c3106e29a6&units=imperial";
+const apiURLf = "http://api.openweathermap.org/data/2.5/forecast?id=5604473&appid=1e25916dc15bb9ee780725c3106e29a6&units=imperial";
 
-fetch(apiURL)
+fetch(apiURLw)
   .then((response) => response.json())
   .then((jsObject) => {
     console.log(jsObject);
 
     document.querySelector('#current-cond').textContent = jsObject.weather[0].description;
-    document.querySelector('#temp').textContent = jsObject.main.temp;
-    document.querySelector('#high').textContent = jsObject.main.temp_max;
+    document.querySelector('#current-temp').innerHTML = jsObject.main.temp.toFixed(0);
+    document.querySelector('#high').textContent = jsObject.main.temp_max.toFixed(0);
     document.querySelector('#humi').textContent = jsObject.main.humidity;
+    document.querySelector('#speed').textContent = jsObject.wind.speed.toFixed(0);
 
-    
+    let temp = jsObject.main.temp;
+    let windSpeed = jsObject.wind.speed;
+    let windChill = (35.74 + (0.6 * temp)) - (35.7 * Math.pow(windSpeed, 0.16)) + (0.43 * temp * Math.pow(windSpeed, 0.16));
+    let windChillTemp = Math.round(windChill);
+    document.getElementById('windchill').textContent = windChillTemp.toFixed(0);
+
+
+  });
+
+  fetch(apiURLf)
+  .then((response) => response.json())
+  .then((jsObject) => {
+    console.log(jsObject);
+
+ 
+
   });
